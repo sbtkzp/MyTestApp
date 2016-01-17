5e6fe3a620f6b3005b811228fe55e2680a1f8e81
sbtkzp <d0_n0t_0ften_use_this@yahoo.co.jp>
2016-01-17 11:58:13 +0900

    DataSet周り試したけどできなかった

--- MyTestApp/Form1.Designer.vb
+++ MyTestApp/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,14 +20,14 @@ Partial Class Form1
     'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
     'Windows フォーム デザイナーを使用して変更できます。  
     'コード エディターを使って変更しないでください。
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(133, 122)
+        Me.Button1.Location = New System.Drawing.Point(13, 12)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
--- MyTestApp/Form1.vb
+++ MyTestApp/Form1.vb
@@ -1,5 +1,9 @@
 ﻿Public Class Form1
+
+    Public Shared bookId As String
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
         MessageBox.Show(Button1.Text)
     End Sub
 End Class
